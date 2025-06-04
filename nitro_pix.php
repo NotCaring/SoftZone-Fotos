dfab1e4850a6dd1ebff825cd73e9b52db5ac1a89
Not Caring <notcaringbyt@gmail.com>
2025-06-04 14:22:32 -0300

    Update nitro_pix.php

--- nitro_pix.php
+++ nitro_pix.php
@@ -1,21 +1,31 @@
 <?php
 // Defina seu token e endpoint
-// TROCAR API DO TOLEM
 $apiToken = 'vsOYr4JtqCu6MUup007y3bhrsIlU6vYeHNLiyO3YoSQDTQyQouQLDEETWrP8';
 $endpoint = 'https://api.nitropagamentos.com/api/public/v1/transactions?api_token=' . $apiToken;
 
-// Dados que você vai receber (exemplo fixo aqui, substitua pelo seu input)
+// Recebe o valor enviado pelo bot
+$valorRecebido = isset($_POST['valor']) ? intval($_POST['valor']) : 0;
+
+// Se não veio valor ou for inválido
+if ($valorRecebido <= 0) {
+    http_response_code(400);
+    echo json_encode([
+        'success' => false,
+        'message' => 'Valor inválido ou não informado.'
+    ]);
+    exit;
+}
+
+// Dados para envio
 $data = [
-    "amount" => 1790,
-    //TROCAR ID DO PRODUTO
+    "amount" => $valorRecebido,
     "offer_hash" => "dclbde2eyp",
-    //
     "payment_method" => "pix",
     "customer" => [
         "name" => "Sou Painho_Dev",
         "email" => "cliente@teste.com",
         "phone_number" => "87991870287",
-        "document" => "12345678900",
+        "document" => "64655673000162", // CPF/CNPJ válido
         "street_name" => "Rua Teste",
         "number" => "123",
         "complement" => "Casa",
@@ -26,11 +36,10 @@ $data = [
     ],
     "cart" => [
         [
-            //TROCAR O HASH DO PRODUTO SERIA O BAGULHO DO CODIGO DO SITE DE PAGAMENTO
-            "product_hash" => "xyszoxbng1",
-            "title" => "Produto Teste API Publica",
+            "product_hash" => "dmlla7dieo",
+            "title" => "Produto comprado no Discord",
             "cover" => null,
-            "price" => 1790,
+            "price" => $valorRecebido,
             "quantity" => 1,
             "operation_type" => 1,
             "tangible" => false
@@ -57,7 +66,6 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 $response = curl_exec($ch);
 
 if (curl_errno($ch)) {
-    // Erro na requisição cURL
     echo json_encode([
         'success' => false,
         'message' => 'Erro na requisição: ' . curl_error($ch)
